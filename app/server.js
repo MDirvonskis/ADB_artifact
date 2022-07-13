203b12bf9941825e0091254baa1b2d9924c17ad9
Martynas Dirvonskis <md597@canterbury.ac.uk>
2022-07-13 06:59:09 +0100

    Created a route and linked up the database.

--- app/server.js
+++ app/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { connect } = require('.database');
+const db = require('./database');//Route for database connection
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
